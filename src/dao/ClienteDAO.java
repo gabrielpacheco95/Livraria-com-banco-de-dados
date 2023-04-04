20d75c2b7165a7f823cf9334c0135a5ea5ca52a1
Gabriel Almeida Pacheco <311101245@senacinformatica.br>
2023-04-04 11:56:50 -0300

    atualizaçao 04/04/2023

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -7,10 +7,10 @@ package dao;
 
 import conexão.Conexao;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 import model.Cliente;
 
 /**
@@ -29,9 +29,50 @@ public class ClienteDAO {
             String sql;
             sql = "insert into clientes values "
                     + "(null, '" + cVO.getNomeCliente() + "','" + cVO.getCpf() + "');";
-            stat.execute (sql); 
+            stat.execute(sql);
         } catch (SQLException ex) {
-            System.out.println("Erro ao cadastrar! \n" + ex.getMessage());            
+            System.out.println("Erro ao cadastrar! \n" + ex.getMessage());
         }
+    }// fim do cadastrar
+
+    public ArrayList<Cliente> getClienteBD() {
+        Connection con = Conexao.getConexao();
+        try {
+            Statement stat = con.createStatement();
+            String sql = "select* from clientes";
+            ResultSet rs = stat.executeQuery(sql);
+            ArrayList<Cliente> clientes = new ArrayList<>();
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                //lado do java |X| Lado do Banco
+                c.setIdCliente(rs.getInt("idcliente"));
+                c.setNomeCliente(rs.getString("nome"));
+                c.setCpf(rs.getString("cpf"));
+                clientes.add(c);
+            }
+            return clientes;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao listar! \n" + ex.getMessage());
+        }
+        return null;
+    }// fim do listar
+
+    public Cliente getClienteByDoc(String cpf) {
+        Connection con = Conexao.getConexao();
+        Cliente c = null;
+        try {
+            Statement stat = con.createStatement();
+            String sql = "select* from clientes where cpf = '" + cpf;
+            ResultSet rs = stat.executeQuery(sql);
+            while (rs.next()) {
+                //lado do java |X| Lado do Banco
+                c.setIdCliente(rs.getInt("idcliente"));
+                c.setNomeCliente(rs.getString("nome"));
+                c.setCpf(rs.getString("cpf"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conultar o CPF!\n" + ex.getMessage());
+        }
+        return c;
     }
 }
